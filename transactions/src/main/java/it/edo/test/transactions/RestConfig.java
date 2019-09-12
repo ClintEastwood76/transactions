d7805545ed3663a126920ecc987b1c7d64b07941
Edoardo C. <registrazioni.edoardo@gmail.com>
2019-09-12 17:44:48 +0200

    ciao

--- transactions/src/main/java/it/edo/test/transactions/RestConfig.java
+++ transactions/src/main/java/it/edo/test/transactions/RestConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class RestConfig {
-    @Bean
+    
+	@Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
