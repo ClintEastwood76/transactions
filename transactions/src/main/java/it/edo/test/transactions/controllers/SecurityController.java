99cfe76624352550b1ef3cfb40d898c133ab37d2
Edoardo C. <registrazioni.edoardo@gmail.com>
2019-09-13 09:50:46 +0200

    ciao

---
+++ transactions/src/main/java/it/edo/test/transactions/controllers/SecurityController.java
@@ -0,0 +1,31 @@
+package it.edo.test.transactions.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+	Logger logger = LoggerFactory.getLogger(SecurityController.class);
+			
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+	
+	@PostMapping("/logout")
+	public boolean logout() {
+		logger.info("Logging out");
+		return true;
+	}
+}
