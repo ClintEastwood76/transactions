74734d6e40da4644538750a204f0316ad90bffe9
Edoardo Campagnano <registrazioni.edoardogmail.com>
2019-10-01 22:10:57 +0200

    rifattorizzazione servizio transazioni

--- transactions/src/main/java/it/edo/test/transactions/services/TransactionService.java
+++ transactions/src/main/java/it/edo/test/transactions/services/TransactionService.java
@@ -2,6 +2,9 @@ package it.edo.test.transactions.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
+import it.edo.test.transactions.domain.Recharge;
 import it.edo.test.transactions.domain.Transaction;
 
 
@@ -10,5 +13,7 @@ public interface TransactionService {
 	public List<Transaction> getAllTransactions(String owner);
 
 	public Transaction getTransaction(String id, String owner);
+	
+	Page<Transaction> getTransactionPage(String owner, Integer pageNum, Integer pageSize);
 
 }
--- transactions/src/main/java/it/edo/test/transactions/services/impl/TransactionServiceImpl.java
+++ transactions/src/main/java/it/edo/test/transactions/services/impl/TransactionServiceImpl.java
@@ -1,12 +1,14 @@
 package it.edo.test.transactions.services.impl;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import it.edo.test.transactions.domain.Transaction;
@@ -14,7 +16,7 @@ import it.edo.test.transactions.repositories.TransactionsRepository;
 import it.edo.test.transactions.services.TransactionService;
 
 @Service
-public class TransactionServiceImpl implements TransactionService {
+public class TransactionServiceImpl extends BasicService implements TransactionService {
 	
 	@Autowired
 	TransactionsRepository repository;
@@ -34,6 +36,19 @@ public class TransactionServiceImpl implements TransactionService {
 		Example<Transaction> example = Example.of(t);
 		return repository.findAll(example).get(0);
 		
+	}
+
+	@Override
+	public Page<Transaction> getTransactionPage(String owner, Integer pageNum, Integer pageSize) {
+		Transaction t = new Transaction();
+		t.setOwner(owner);
+		Example<Transaction> example = Example.of(t);
+		Pageable pageable = PageRequest.of(
+				getPageNum(pageNum), 
+				getPageSize(pageSize),
+				Sort.by("date").descending()
+			);
+		return repository.findAll(example, pageable);
 	}
 	
 	
